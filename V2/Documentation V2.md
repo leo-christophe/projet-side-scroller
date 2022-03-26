0212a6432373853b64dfc280a0f06c0c33a3b4ab
Leo C <leolerenardo@gmail.com>
2022-03-26 14:51:33 +0100

    reorganisation

---
+++ V2/Documentation V2.md
@@ -0,0 +1,59 @@
+# Présentation
+- Ce jeu est un **side-scroller** dans lequel on progresse dans un **environnement 2D**. Le jeu se décomposera en **5 "mondes"**. Le premier aura un univers assez *campagnard*, les plaines et les champs. Le deuxième aura un univers *désertique*, le sable et la sécheresse. Le troisième aura un univers *montagnard*, neigeux et froid. Le quatrième aura un univers très chaud, semblable *aux enfers* et enfin, le cinquième sera *le Boss finale* du jeu. Ce jeu aura donc une fin : le boss. 
+- Dans le but d'y arriver le joueur aura la possibilité **de récupérer des équipements**, **d'augmenter son niveau**, sa vie, sa force. Il pourra aussi acheter ces équipements dans des boutiques à l'aide de pièces.
+
+
+# Changelog 
+## V2:
+-
+
+## V1:
+- Le joueur est maintenant géré à l'aide de la classe `Sprite_Player`.
+- Changement de méthode pour les collisions avec `colliderect`. Les collisions sont maintenant beaucoup plus efficaces.
+- Changement de **toutes les textures** et **ajout de textures** *(qui sont maintenant au nombre de 39) 18 pour le joueur lorsqu'il marche à gauche, 18 pour le joueur lorsqu'il marche à droite, 1 lorsqu'il saute, 1 lorsqu'il ne fait rien et 1 lorsqu'il regarde en l'air.*
+=> Le joueur est maintenant d'une résolution de 32x64. 
+- Ajout d'une animation pour le joueur avec la fonction `redrawWindow`
+- Ajout d'une musique de fond et de sons de marche (facultatifs). 
+#### Controles :
+- Ajout de controles en plus des flèches:
+> `q` pour aller à gauche, `d` pour aller à droite, `z` pour regarder en l'air, `la barre d'espace` pour sauter.
+- Modification du fonctionnement du saut. Si le joueur regarde à droite, il va sauter vers la droite, s'il regarde à gauche, il va sauter vers la gauche, sinon, s'il regarde en l'air (z) ou s'il reste normal (debout), il saute tout droit. 
+- Ajout de documentations, de commentaires, debut de la fiche probleme-solution, optimisation du code.
+
+### Réparation de bugs:
+- Saut à l'infini.
+---
+## V0:
+- Ajout du joueur
+- Ajout de collisions pour empêcher le joueur de dépasser les limites
+- Ajout de gravité pour que le joueur redescende après avoir sauté.
+- Ajout des controles pour que le joueur puisse sauter et se déplacer.
+- Ajout de textures pour le joueur et l'arrière plan
+- Ajout d'un moyen de quitter
+
+# Conception :
+- L'interface graphique est gérée avec Pygame. 
+_Le programme est construit en 2 grandes parties :_
+1. **L'initialisation** : Dans cette première partie, les variables, fonctions, classes sont initialisées. ça permet de "désengorger" la boucle principale du jeu qui tournera beaucoup de fois pendant plus ou moins longtemps. Dans cette partie, on a une classe : `Sprite_Player` qui gère le joueur, une fonction `redrawWindow` qui gère l'animation et on initialise plein de variables qui serviront pour la 2ème partie. 
+2. **La boucle principale** : Elle permet le fonctionnement du jeu. En répétant la même boucle beaucoup de fois en une seconde, on fait en sorte de mettre à jour les affichages, après avoir fait bouger le personnage par exemple. On procède à chaque tour de boucle de la même façon : on change certaines choses comme la position du joueur ou sa texture, on met à jour, ça s'affiche. 
+
+### Les images :
+Il faut savoir que positionner les images n'est pas si facile. En effet, les positions sont assez spéciales. 
+![](https://i.imgur.com/3ldvk3N.png)
+La position initiale, (0,0) se trouve en haut à gauche de la fenêtre. Donc, si on descend, y augmente et si on va à droite, x augmente. À l'inverse, si on monte, y baisse et si on va à gauche, x baisse. 
+
+### L'animation :
+Elle est gérée grace à la fonction `redrawWindow()` qui est peut être traduit par "redessinage de la fenêtre". En effet, cette fonction permet d'effectuer une animation par rapport au joueur. Elle se sert de la variable `walkCount` comptant les "pas" du joueur. A chaque pas, la fonction va détecter si le joueur marche vers la gauche grace à `left` ou la droite grace à `right` et va afficher l'animation correspondante en prenant dans le tableau right_assets ou left_assets correspondant l'indice `walkCount`. Pour le saut, on détecte si le joueur est en train de sauter grace à `isJump`. 
+**Il y a donc des variables ayant comme donnée des booléens pour chaque "stade" d'animation:** 
+* >`right` pour signaler que le joueur va à droite.
+* >`left` pour signaler que le joueur va à gauche.
+* >`isJump` pour signaler que le joueur saute.
+* >`isLookingUp` pour signaler que le joueur regarde en haut.
+* >`isStanding` pour signaler que le joueur se tient juste debout.
+
+# Idées futures / Ambitions pour la V2 :
+- Un défilement vers la gauche tel un Mario. 
+- La possibilitée de changer d'univers. (les 4 mondes) 
+- Créer des ennemis et donc un moyen de les battre.
+=> cela implique un moyen de perdre donc un écran "Game Over", un moyen de mourir de chute ou lorsqu'on saute dans le vide? 
+- Ajouter un menu.
---
+++ V2/main.py
@@ -0,0 +1,275 @@
+import pygame
+from pygame import mixer
+from random import randint
+pygame.init()
+
+#chargement de la fenêtre
+WIDTH = 1080
+HEIGHT = 720
+taille = (WIDTH, HEIGHT)
+fenetre = pygame.display.set_mode(taille)   
+pygame.display.set_caption("Juan Adventures") 
+icon = pygame.image.load("assets/textures/player/player.png")
+pygame.display.set_icon(icon) #on change l'icone de la fenêtre
+all_platforms = []
+
+bg = pygame.image.load("assets/textures/background/fond_plaines.png").convert()
+fond_plaine_plateforme = pygame.image.load("assets/textures/background/fond_plaines_plateforme.png")
+fond_plaine_plateforme_pos = fond_plaine_plateforme.get_rect()
+all_platforms.append(fond_plaine_plateforme_pos)
+fond_plaine_plateforme_pos.y = 546 #la position y de la plateforme qui du bas qui va servir de collision
+
+clock = pygame.time.Clock() #on va pouvoir changer les fps
+
+class Sprite_Player(pygame.sprite.Sprite):
+    """
+    La classe Sprite_Player prenant en argument pygame.sprite.Sprite est la classe permettant du joueur. Elle permet de récupérer des informations sur ce dernier,
+    d'en modifier...
+    """
+    marge_x = 3
+    marge_y = 80
+    def __init__(self):
+        """
+        La méthode __init__ permet d'initialiser le joueur. 
+        """
+        super().__init__() #appel du constructeur de la classe parente Sprite
+        pygame.sprite.Sprite.__init__(self)
+
+        #? image et coordonnées
+
+        self.image = pygame.image.load("assets/textures/player/player.png")
+        self.jumping = pygame.image.load("assets/textures/player/player_jumping.png")
+        self.looking_up = pygame.image.load("assets/textures/player/player_looking_up.png")
+        self.right_assets = [pygame.image.load(f'assets/textures/player/player_right_{x}.png') for x in range(1, 19)]
+        self.left_assets = [pygame.image.load(f'assets/textures/player/player_left_{x}.png') for x in range(1, 19)]
+        
+        self.left = False
+        self.right = False
+        self.isJump = False
+        self.isLookingUp = False
+        self.isStanding = False
+        self.walkCount = 0
+
+        self.size = self.image.get_size()
+        self.rect = self.image.get_rect()
+        self.rect.x = self.size[0]
+        self.rect.y = 720 - 150 - self.size[1]
+
+        #? statistiques
+        self.speed = 3
+        self.vie = 100
+        self.att = 1
+        self.zone = 1 #? la zone du joueur
+
+    def goRight(self, marge):
+        """
+        La méthode goRight permet de déplacer le joueur de marge pixels vers la droite. 
+        Pré-Condition : La méthode goRight prend en argument marge.
+        Post-Condition : Elle retourne self.rect, la position du joueur. 
+        """
+        self.rect.x += marge * self.speed
+        if grass_sound == True:
+            son = grass_steps[randint(0,2)]
+            son.set_volume(0.25)
+            son.play()
+        return self.rect
+
+    def goLeft(self, marge):
+        """
+        La méthode goLeft permet de déplacer le joueur de marge pixels vers la gauche. 
+        Pré-Condition : La méthode goLeft prend en argument marge.
+        Post-Condition : Elle retourne self.rect, la position du joueur. 
+        """
+        self.rect.x -= marge * self.speed
+        if grass_sound == True:
+            son = grass_steps[randint(0,2)]
+            son.set_volume(0.25)
+            son.play()
+        return self.rect
+
+    def goJump(self, jump = 60):
+        """
+        La méthode goJump permet de faire sauter le joueur de jump pixels. 
+        Pré-Condition : Elle prend en argument jump, qui sera pratiquemment tout le temps à 150.
+        left permet de faire sauter le joueur vers la gauche et right permet de faire sauter le joueur vers la droite.
+        Post-Condition : Elle retourne self.rect, la position du joueur. 
+        """
+        if self.rect.colliderect(fond_plaine_plateforme_pos) == True:
+            decalement_x = 120
+            if self.isLookingUp or self.isStanding:
+                self.rect.y -= jump * 1.5
+            else:
+                if self.left == True:
+                    decalement_x = -(decalement_x) #? On change decalement_x en négatif s'il faut partir vers la gauche, sinon pas besoin (puisqu'il est positif)
+                if self.left == False and self.right == False:
+                    decalement_x = 0
+                self.rect.y -= jump
+                self.rect.x += decalement_x
+        return self.rect
+
+    def soin(self, soin):
+        """
+        Cette méthode permet de soigner le joueur.
+        Pré-Condition : soin est un float correspondant aux points de vie ajoutés à la vie du joueur.
+        """
+        assert type(soin) == float
+        if self.soin < 100:
+            self.soin += soin
+        else:
+            print("Il n'y a rien à soigner.")
+
+    def degat(self, degats):
+        """
+        Cette méthode permet d'infliger des dégâts au joueur.
+        Pré-Condition : degats est un float correspondant aux points de dégat infligés au joueur, enlevant de la vie. 
+        """
+        assert type(degats) == float
+        self.vie -= degats
+        if self.vie < 0:
+            self.vie = 0
+current_player = Sprite_Player()
+
+music_playing = True
+grass_sound = False
+grass_steps = [mixer.Sound(f'assets/sounds/ambiant/grass-step-{i}.mp3') for i in range(1, 4)]
+def music():
+    if music_playing == True: #jouer de la musique
+        mixer.music.load(f"assets/sounds/music/{current_player.zone}/grassy_plains-darren_curtis.mp3")
+        mixer.music.play(-1)
+
+
+def redrawWindow():
+    """
+    Cette fonction sert à faire les animations.
+    Pré-Conditions : left et right sont des booléens indiquant la position du joueur (vers la droite ou vers la gauche)
+    """
+
+    fenetre.fill((0, 0, 0))
+    fenetre.blit(fond_plaine_plateforme, fond_plaine_plateforme_pos)
+    fenetre.blit(bg, (0,0))
+
+    pos = (current_player.rect.x, current_player.rect.y)
+
+    anim_left = current_player.left
+    anim_right = current_player.right
+    anim_jump = current_player.isJump
+    anim_lookup = current_player.isLookingUp
+
+    if current_player.walkCount <= len(current_player.left_assets)-1: 
+        if anim_left: #on utilise chaque élément du tableau left_assets
+            fenetre.blit(
+                current_player.left_assets[current_player.walkCount], 
+                pos)
+            current_player.walkCount +=1
+
+        elif anim_right: #on utilise chaque élément du tableau right_assets
+            fenetre.blit(
+                current_player.right_assets[current_player.walkCount], 
+                pos)
+            current_player.walkCount += 1
+
+        elif anim_jump:
+            fenetre.blit(current_player.jumping, pos)
+
+        elif anim_lookup:
+            fenetre.blit(current_player.looking_up, pos)
+        else:
+            fenetre.blit(current_player.image, current_player.rect)
+        
+    else:
+        current_player.walkCount = 0
+
+
+def jeu(fps = 60):
+    """
+    Cette fonction est la fonction principale du jeu, elle contient la boucle qui le fait tourner. Elle prend en argument fps, qui sera habituellement à 60. 
+    Cette boucle infinie sert à faire marcher le jeu en continu. Tant que ALT-F4 ou que la fenêtre n'est pas fermée, le jeu continuera de marcher dès l'exécution du programme.
+    """
+    jeu = True
+    while jeu:
+        #! dans ces evenements, on ne peut pas rester appuyer sur la touche pour répéter l'évenement, il ne se produit qu'une fois à chaque touche appuyée. 
+        for event in pygame.event.get(): #!pour quitter
+            if event.type == pygame.QUIT:
+                jeu = False
+                pygame.quit()
+                quit()
+            
+            elif event.type == pygame.KEYDOWN: #! sauter
+                if (event.key == pygame.K_UP) or (event.key == pygame.K_SPACE):
+                    current_player.isJump = True
+                    current_player.walkCount = 0
+                    current_player.goJump()
+                else:
+                    current_player.isJump = False
+                    current_player.walkCount = 0
+        
+        #! dans ces evenements, on peut rester avec la touche appuyée pour continuer l'evenement qui s'arrête une fois que la touche n'est plus appuyée. 
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] or keys[pygame.K_q]: #!gauche
+            current_player.right = False
+            current_player.left = True
+            current_player.isLookingUp = False
+            current_player.isStanding = False
+            current_player.isJump = False
+
+            current_player.goLeft(Sprite_Player.marge_x)
+
+        elif keys[pygame.K_RIGHT] or keys[pygame.K_d]: #!droite
+            current_player.left = False
+            current_player.right = True
+            current_player.isLookingUp = False
+            current_player.isStanding = False
+            current_player.isJump = False
+
+            current_player.goRight(Sprite_Player.marge_x)
+
+        elif keys[pygame.K_z]: #!regarder en haut (animation)
+            current_player.isLookingUp = True
+            current_player.isStanding = False
+            current_player.right = False
+            current_player.left = False
+            current_player.isJump = False
+
+        else:
+            current_player.isStanding = False
+            current_player.isLookingUp = False
+            current_player.isJump = False
+            current_player.walkCount = 0
+
+        #! COLLISIONS ET GRAVITÉ
+        if current_player.rect.colliderect(fond_plaine_plateforme_pos) == True: #! Collisions d'en bas
+            current_player.rect.bottom = fond_plaine_plateforme_pos.top
+
+        if current_player.rect.x >= 1080 - current_player.size[1]:             #! Collisions d'à droite
+            current_player.rect = current_player.rect.move(-10, 0)
+
+        elif current_player.rect.x <= 0:                                       #! Collisions d'à gauche
+            current_player.rect = current_player.rect.move(10, 0)
+
+        elif not(current_player.rect.colliderect(fond_plaine_plateforme_pos)): #! Gravité
+            current_player.rect = current_player.rect.move(0, 5)
+
+        redrawWindow() #animations
+        
+        pygame.display.flip() #rafraichissement de la fenêtre
+
+        clock.tick(fps) #change les fps du jeu.
+    pygame.quit()
+if __name__ == "__main__":
+    music()
+    jeu()
+
+#(Des liens pour des tutos)
+#TODO https://pygame-gui.readthedocs.io/en/latest/quick_start.html
+#TODO http://sdz.tdct.org/sdz/interface-graphique-pygame-pour-python.html 
+#TODO https://coderslegacy.com/python/pygame-gravity-and-jumping/
+#TODO #https://www.techwithtim.net/tutorials/game-development-with-python/pygame-tutorial/pygame-animation/
+#TODO pygame.transform très intéréssant
+#TODO SONS https://www.youtube.com/watch?v=pcdB2s2y4Qc
+#TODO défilement http://programarcadegames.com/python_examples/f.php?file=platform_scroller.py
+#TODO MENU https://www.reddit.com/r/pygame/duplicates/rwbfl3/make_a_menu_in_pygame_and_python_in_5_min_using/
+#TODO https://www.geeksforgeeks.org/how-to-create-a-text-input-box-with-pygame/?ref=rp
+#TODO PLATEFORMES QUI BOUGENT http://programarcadegames.com/python_examples/f.php?file=platform_moving.py
+#TODO Examples cool http://programarcadegames.com/index.php?chapter=introduction_to_sprites
+#TODO https://sciences-du-numerique.fr/tuto-pygame/sprites.html
+
